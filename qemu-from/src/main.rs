5e563df9e768c4c57784f326a80908ef26be5a8c
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-04 21:08:34 +0900

    snippets: Can parse EAX=0000aa55 into tuple. #1

--- qemu-from/src/main.rs
+++ qemu-from/src/main.rs
@@ -1,6 +1,6 @@
 extern crate combine;
 use combine::{Parser, many1, token, sep_by};
-use combine::char::{hex_digit, letter, alpha_num};
+use combine::char::{hex_digit, letter, alpha_num, spaces};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum CpuRegister {
@@ -14,4 +14,16 @@ fn main() {
 
     let mut parser = sep_by::<Vec<String>, _, _>(many1::<String, _>(alpha_num()), token('='));
     assert_eq!(parser.parse("EAX=0000aa55"), Ok((vec!["EAX".to_string(), "0000aa55".to_string()], "")));
+
+    let mut parser = sep_by(many1::<String, _>(letter().or(token('=')).or(alpha_num())), spaces());
+    assert_eq!(parser.parse("EAX=0000aa55 EBX=00000000"), Ok((vec!["EAX=0000aa55".to_string(), "EBX=00000000".to_string()], "")));
+
+    let mut register = sep_by::<Vec<String>, _, _>(many1::<String, _>(alpha_num()), token('='))
+        .map(|reg| CpuRegister::General(reg[0].clone(), u32::from_str_radix(&reg[1], 16).unwrap()));
+    assert_eq!(register.parse("EAX=0000aa55"), Ok((CpuRegister::General("EAX".to_string(), 0xaa55u32), "")));
+
+    let mut id = many1::<String, _>(letter()).skip(token('='));
+    let mut value = many1::<String, _>(hex_digit()).map(|value| u32::from_str_radix(&value, 16).unwrap());
+    let res = id.parse("EAX=0000aa55").map(|x| CpuRegister::General(x.0, value.parse(x.1).unwrap().0));
+    assert_eq!(res, Ok(CpuRegister::General("EAX".to_string(), 0x0000aa55u32)));
 }
