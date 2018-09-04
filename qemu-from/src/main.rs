c131801f105cdad0466ebb60cfcde1b9a613dbf7
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-04 09:57:52 +0900

    snippets: parse =0000aa55. #1

--- qemu-from/src/main.rs
+++ qemu-from/src/main.rs
@@ -1,9 +1,9 @@
 extern crate combine;
-use combine::Parser;
-use combine::char::letter;
+use combine::{Parser, many1, token};
+use combine::char::{hex_digit};
 
 fn main() {
-    let mut word = letter();
-    let result = word.parse("test");
-    assert!(result.is_ok());
+    let mut parser = token('=')
+        .with(many1(hex_digit()).map(|s: String| u32::from_str_radix(&s, 16).unwrap()));
+    assert_eq!(parser.parse("=0000aa55"), Ok((0x0000aa55u32, "")));
 }
