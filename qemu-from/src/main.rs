c9d564828637dc361f359bfa2bda361151ca6121
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-04 10:35:20 +0900

    snippets: Parse EAX=0000aa55. #1

--- qemu-from/src/main.rs
+++ qemu-from/src/main.rs
@@ -1,9 +1,17 @@
 extern crate combine;
-use combine::{Parser, many1, token};
-use combine::char::{hex_digit};
+use combine::{Parser, many1, token, sep_by};
+use combine::char::{hex_digit, letter, alpha_num};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum CpuRegister {
+    General(String, u32),
+}
 
 fn main() {
     let mut parser = token('=')
         .with(many1(hex_digit()).map(|s: String| u32::from_str_radix(&s, 16).unwrap()));
     assert_eq!(parser.parse("=0000aa55"), Ok((0x0000aa55u32, "")));
+
+    let mut parser = sep_by::<Vec<String>, _, _>(many1::<String, _>(alpha_num()), token('='));
+    assert_eq!(parser.parse("EAX=0000aa55"), Ok((vec!["EAX".to_string(), "0000aa55".to_string()], "")));
 }
