56fd93738ae75aa034c7c3b04b12e8a19a81ff21
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-05 20:52:17 +0900

    snippets: Use parser of parser tuple. #1

--- qemu-from/src/main.rs
+++ qemu-from/src/main.rs
@@ -29,8 +29,12 @@ fn main() {
         .map(|x| CpuRegister::General(x.0, value.parse(x.1).unwrap().0));
     assert_eq!(res, Ok(CpuRegister::General("EAX".to_string(), 0x0000aa55u32)));
 
-    let mut parser = sep_by::<Vec<String>, _, _>(
-        many1::<String, _>(letter().or(token('=')).or(alpha_num())), spaces());
-    let res = parser.parse("EAX=0000aa55 EBX=00000000")
-        .map(|regs| for r in regs.0 {id.parse(&r).map(|x| CpuRegister::General(x.0, value.parse(x.1).unwrap().0)); } );
+    let id = many1::<String, _>(letter()).skip(token('='));
+    let value = many1::<String, _>(hex_digit());
+    let mut register = (id, value)
+        .map(|(id, value)| {
+            CpuRegister::General(id, u32::from_str_radix(&value, 16).unwrap())
+        });
+
+    assert_eq!(register.parse("EAX=0000aa55"), Ok((CpuRegister::General("EAX".to_string(), 0x0000aa55u32), "")));
 }
