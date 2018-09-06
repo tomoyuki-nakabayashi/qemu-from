fea26e19b7c9710f7142cbaea753d01c0faa53b4
Your Name <you@example.com>
2018-09-06 13:23:12 +0900

    snippets: Implementing segment register parse.

--- qemu-from/src/main.rs
+++ qemu-from/src/main.rs
@@ -5,9 +5,14 @@ use combine::char::{hex_digit, letter, spaces};
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum CpuRegister {
     General(String, u32),
+    Segment(String, u64, u64, u64, u64),
 }
 
 fn main() {
+}
+
+fn parse_segment_register(line: &str) -> CpuRegister {
+    CpuRegister::Segment("ES".to_string(), 0, 0, 0xffff, 0x9300)
 }
 
 fn parse_general_register(line: &str) -> Vec<CpuRegister> {
@@ -31,5 +36,18 @@ mod test {
         assert_eq!(parse_general_register("EAX=0000aa55 EBX=00000000"),
             vec![CpuRegister::General("EAX".to_string(), 0x0000aa55u32),
                 CpuRegister::General("EBX".to_string(), 0x00000000u32)]);
+    }
+
+    #[test]
+    fn segment_register() {
+        assert_eq!(parse_segment_register("ES =0000 00000000 0000ffff 00009300"),
+            CpuRegister::Segment("ES".to_string(), 0, 0, 0xffff, 0x9300));
+    }
+
+    #[test]
+    fn split_segment() {
+        let mut parser = many1::<String, _>(letter()).skip(spaces()).skip(token('='));
+        let hex = many1::<String, _>(hex_digit());
+        assert_eq!(parser.parse("ES ="), Ok(("ES".to_string(), "")));
     }
 }
