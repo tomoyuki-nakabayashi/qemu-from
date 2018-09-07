685b176d13d86d9b6874aa9d66cd477082944b71
Your Name <you@example.com>
2018-09-07 13:26:33 +0900

    snippets: Return tuple from sep_by.

--- qemu-from/src/main.rs
+++ qemu-from/src/main.rs
@@ -8,14 +8,14 @@ mod register_parser;
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum CpuRegister {
     General(String, u32),
-    Segment(String, u64, u64, u64, u64),
+    Segment(String, (u64, u64, u64, u64)),
 }
 
 fn main() {
 }
 
 fn parse_segment_register(line: &str) -> CpuRegister {
-    CpuRegister::Segment("ES".to_string(), 0, 0, 0xffff, 0x9300)
+    CpuRegister::Segment("ES".to_string(), (0, 0, 0xffff, 0x9300))
 }
 
 fn parse_general_register(line: &str) -> Vec<CpuRegister> {
@@ -44,21 +44,22 @@ mod test {
     #[test]
     fn segment_register() {
         assert_eq!(parse_segment_register("ES =0000 00000000 0000ffff 00009300"),
-            CpuRegister::Segment("ES".to_string(), 0, 0, 0xffff, 0x9300));
+            CpuRegister::Segment("ES".to_string(), (0, 0, 0xffff, 0x9300)));
     }
 
     #[test]
     fn split_segment() {
+        use itertools::Itertools;
         let id = many1::<String, _>(letter()).skip(spaces()).skip(token('='));
         let hex = many1::<String, _>(hex_digit())
             .map(|h| u64::from_str_radix(&h, 16).unwrap());
-        let hex_list = sep_by::<Vec<u64>, _, _>(hex, spaces());
+        let hex_list = sep_by::<Vec<u64>, _, _>(hex, spaces())
+            .map(|hexes| hexes.into_iter().tuples::<(_,_,_,_)>().next().unwrap());
 
         let mut parser = (id, hex_list);
         assert_eq!(parser.parse("ES =0000 00000000 0000ffff 00009300"),
-            Ok((("ES".to_string(), vec![0, 0, 0xffff, 0x9300]), "")));
+            Ok((("ES".to_string(), (0, 0, 0xffff, 0x9300)), "")));
 
-        use itertools::Itertools;
         let mut t = vec![0u64, 0, 0xffff, 0x9300].into_iter().tuples::<(_, _, _, _)>();
         assert_eq!(t.next(), Some((0u64, 0, 0xffff, 0x9300)));
     }
