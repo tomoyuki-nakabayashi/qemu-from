3d41a0c22317561196f1a9f389c65a72f89ba666
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-10 21:34:45 +0900

    refactor: Return values of parser.

--- qemu-from/src/main.rs
+++ qemu-from/src/main.rs
@@ -10,10 +10,13 @@ use combine::parser::repeat::skip_until;
 mod register_parser;
 
 #[derive(Debug, PartialEq)]
-pub(crate) struct GeneralRegister (String, u64);
+pub(crate) struct GeneralRegister (usize, u64);
 
 #[derive(Debug, PartialEq)]
 pub(crate) struct SegmentRegister (String, (u64, u64, u64, u64));
+
+#[derive(Debug, PartialEq)]
+pub(crate) struct HFlag (String, u64);
 
 #[derive(Debug, PartialEq)]
 struct SegmentRegisters {
@@ -45,10 +48,10 @@ struct ControlRegs {
 
 #[derive(Debug, PartialEq)]
 struct StatusRegisters {
-    EIP: GeneralRegister,
-    EFLAGS_RAW: GeneralRegister,
+    EIP: u64,
+    EFLAGS_RAW: u64,
     EFLAGS: Vec<char>,
-    HFLAGS: Vec<GeneralRegister>,
+    HFLAGS: Vec<HFlag>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -61,76 +64,33 @@ struct Cpu {
     efer: u64,
 }
 
-// This will be removed.
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum CpuRegister {
-    General(String, u32),
-    Segment(String, (u64, u64, u64, u64)),
-}
-
 fn main() {
 }
 
-fn parse_general_register(line: &str) -> Vec<CpuRegister> {
-    let id = many1::<String, _>(letter()).skip(token('='));
-    let value = many1::<String, _>(hex_digit());
-    let register = (id, value)
-        .map(|(id, value)| {
-            CpuRegister::General(id, u32::from_str_radix(&value, 16).unwrap())
-        });
-
-    let mut parser = sep_by::<Vec<CpuRegister>, _, _>(register, spaces());
-    parser.parse(line).unwrap().0
-}
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use register_parser::{gpr_parser, eflags_parser};
-
-    #[test]
-    fn general_register() {
-        assert_eq!(parse_general_register("EAX=0000aa55 EBX=00000000"),
-            vec![CpuRegister::General("EAX".to_string(), 0x0000aa55u32),
-                CpuRegister::General("EBX".to_string(), 0x00000000u32)]);
-    }
-
-    #[test]
-    fn split_segment() {
-        use itertools::Itertools;
-        let id = many1::<String, _>(letter()).skip(spaces()).skip(token('='));
-        let hex = many1::<String, _>(hex_digit())
-            .map(|h| u64::from_str_radix(&h, 16).unwrap());
-        let hex_list = sep_by::<Vec<u64>, _, _>(hex, spaces())
-            .map(|hexes| hexes.into_iter().tuples::<(_,_,_,_)>().next().unwrap());
-
-        let mut parser = (id, hex_list)
-            .map(move |(id, d)| CpuRegister::Segment(id, d));
-
-        let res = parser.parse("ES =0000 00000000 0000ffff 00009300");
-        assert_eq!(res, Ok((CpuRegister::Segment("ES".to_string(), (0, 0, 0xffff, 0x9300)), "")));
-    }
+    use register_parser::{gpr_parser, eflags_parser, hflag_parser, qword_parser};
 
     #[test]
     fn status_registers() {
         let mut parser = struct_parser!{
             StatusRegisters {
-                EIP: gpr_parser(),
-                _: spaces(),
-                EFLAGS_RAW: gpr_parser(),
-                _: spaces(),
+                EIP: qword_parser(),
+                EFLAGS_RAW: qword_parser(),
                 EFLAGS: eflags_parser(),
                 _: spaces(),
-                HFLAGS: sep_by(gpr_parser(), spaces()),
+                HFLAGS: sep_by(hflag_parser(), spaces()),
             }
         };
 
         let res = parser.parse("EIP=00007c00 EFL=00000202 [-------] CPL=0 II=0 A20=1 SMM=0 HLT=0");
         assert_eq!(res.unwrap(), (StatusRegisters{
-            EIP: gpr_parser().parse("EIP=00007c00").unwrap().0,
-            EFLAGS_RAW: gpr_parser().parse("EFL=00000202").unwrap().0,
+            EIP: qword_parser().parse("EIP=00007c00").unwrap().0,
+            EFLAGS_RAW: qword_parser().parse("EFL=00000202").unwrap().0,
             EFLAGS: eflags_parser().parse("[-------]").unwrap().0,
-            HFLAGS: sep_by(gpr_parser(), spaces()).parse("CPL=0 II=0 A20=1 SMM=0 HLT=0").unwrap().0 
+            HFLAGS: sep_by(hflag_parser(), spaces()).parse("CPL=0 II=0 A20=1 SMM=0 HLT=0").unwrap().0 
             },
             ""));
     }
--- qemu-from/src/register_parser.rs
+++ qemu-from/src/register_parser.rs
@@ -1,5 +1,6 @@
-use ::{GeneralRegister, SegmentRegister};
+use ::{GeneralRegister, SegmentRegister, HFlag};
 use combine::{Parser, Stream, many1, token, sep_by, one_of, between};
+use combine::parser::repeat::skip_until;
 use combine::char::{hex_digit, letter, spaces, alpha_num};
 use combine::error::ParseError;
 extern crate itertools;
@@ -26,7 +27,24 @@ pub(crate) fn gpr_parser<I>() -> impl Parser<Input = I, Output = GeneralRegister
     let value = many1::<String, _>(hex_digit());
     let parser = (id, value)
         .map(|(id, value)| {
-            GeneralRegister(id, u64::from_str_radix(&value, 16).unwrap())
+            let id = reg_number_from_id(&id).expect("Invalid register id.");
+            let value = u64::from_str_radix(&value, 16).expect("Fail to convert to u64");
+            GeneralRegister(id, value)
+        });
+
+    parser
+}
+
+pub(crate) fn qword_parser<I>() -> impl Parser<Input = I, Output = u64>
+    where I: Stream<Item = char>,
+          I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    let _id = spaces().with(skip_until(token('=')).skip(token('=')));
+    let value = many1::<String, _>(hex_digit());
+    let parser = (_id, value)
+        .map(|(_id, value)| {
+            let value = u64::from_str_radix(&value, 16).expect("Fail to convert to u64");
+            value
         });
 
     parser
@@ -49,12 +67,23 @@ fn segment_parser<I>() -> impl Parser<Input = I, Output = SegmentRegister>
     parser
 }
 
+pub(crate) fn hflag_parser<I>() -> impl Parser<Input = I, Output = HFlag>
+    where I: Stream<Item = char>,
+          I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    let id = many1::<String, _>(alpha_num()).skip(token('='));
+    let value = many1::<String, _>(hex_digit()).map(|h| u64::from_str_radix(&h, 16).unwrap());
+    let parser = (id, value).map(|(id, value)| { HFlag(id, value) });
+
+    parser
+}
+
 pub(crate) fn eflags_parser<I>() -> impl Parser<Input = I, Output = Vec<char>>
     where I: Stream<Item = char>,
           I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let eflag = many1::<Vec<char>, _>(one_of("DOSZAPC-".chars()));
-    let parser = between(token('['), token(']'), eflag);
+    let parser = spaces().with(between(token('['), token(']'), eflag));
 
     parser
 }
@@ -66,12 +95,18 @@ mod test {
     #[test]
     fn get_general_register() {
         let result = gpr_parser().parse("EAX=0000aa55");
-        assert_eq!(result, Ok((GeneralRegister("EAX".to_string(), 0xaa55u64), "")));
+        assert_eq!(result, Ok((GeneralRegister(0 as usize, 0xaa55u64), "")));
     }
 
     #[test]
     fn get_segment_register() {
         let result = segment_parser().parse("ES =0000 00000000 0000ffff 00009300");
         assert_eq!(result, Ok((SegmentRegister("ES".to_string(), (0, 0, 0xffff, 0x9300)), "")));
+    }
+
+    #[test]
+    fn qword() {
+        let result = qword_parser().parse("EIP=00007c00");
+        assert_eq!(result, Ok((0x7c00, "")));
     }
 }
