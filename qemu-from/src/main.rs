433b77c9d9b831638c6bf09a7a135e04c322134b
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-15 22:11:45 +0900

    refactor: format.

--- qemu-from/src/main.rs
+++ qemu-from/src/main.rs
@@ -7,17 +7,16 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
-use combine::{Parser, Stream, count};
-use combine::char::{newline};
-use combine::parser::repeat::{skip_until};
+use combine::char::newline;
 use combine::error::ParseError;
+use combine::parser::repeat::skip_until;
+use combine::{count, Parser, Stream};
 
 mod register_parser;
-use register_parser::{
-        eflags_parser, hflag_parser, qword_parser, segment_parser, dt_parser};
+use register_parser::{dt_parser, eflags_parser, hflag_parser, qword_parser, segment_parser};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub(crate) struct HFlag (String, u64);
+pub(crate) struct HFlag(String, u64);
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct GeneralRegisters {
@@ -32,8 +31,9 @@ struct GeneralRegisters {
 }
 
 fn general_regs_parser<I>() -> impl Parser<Input = I, Output = GeneralRegisters>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let parser = struct_parser!{
         GeneralRegisters {
@@ -64,8 +64,9 @@ struct SegmentRegisters {
 }
 
 fn segment_regs_parser<I>() -> impl Parser<Input = I, Output = SegmentRegisters>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let parser = struct_parser!{
         SegmentRegisters {
@@ -90,8 +91,9 @@ struct DescriptorTable {
 }
 
 fn descriptor_tables_parser<I>() -> impl Parser<Input = I, Output = DescriptorTable>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let parser = struct_parser!{
         DescriptorTable {
@@ -103,7 +105,6 @@ fn descriptor_tables_parser<I>() -> impl Parser<Input = I, Output = DescriptorTa
     parser
 }
 
-
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct ControlRegs {
     CR0: u64,
@@ -113,8 +114,9 @@ struct ControlRegs {
 }
 
 fn control_regs_parser<I>() -> impl Parser<Input = I, Output = ControlRegs>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let parser = struct_parser!{
         ControlRegs {
@@ -139,8 +141,9 @@ struct DebugRegs {
 }
 
 fn debug_regs_parser<I>() -> impl Parser<Input = I, Output = DebugRegs>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let parser = struct_parser!{
         DebugRegs {
@@ -165,8 +168,9 @@ struct StatusRegisters {
 }
 
 fn status_regs_parser<I>() -> impl Parser<Input = I, Output = StatusRegisters>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let parser = struct_parser!{
         StatusRegisters {
@@ -193,7 +197,8 @@ struct Cpu {
 }
 
 fn cpu_state_parser<I>() -> impl Parser<Input = I, Output = Cpu>
-    where I: Stream<Item = char>,
+where
+    I: Stream<Item = char>,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let parser = struct_parser!{
@@ -214,7 +219,8 @@ fn cpu_state_parser<I>() -> impl Parser<Input = I, Output = Cpu>
 
 fn main() {
     let mut parser = cpu_state_parser();
-    let res = parser.parse("EAX=0000aa55 EBX=00000000 ECX=00000000 EDX=00000080
+    let res = parser.parse(
+        "EAX=0000aa55 EBX=00000000 ECX=00000000 EDX=00000080
 ESI=00000000 EDI=00000000 EBP=00000000 ESP=00006f2c
 EIP=00007c00 EFL=00000202 [-------] CPL=0 II=0 A20=1 SMM=0 HLT=0
 ES =0000 00000000 0000ffff 00009300
@@ -231,13 +237,12 @@ CR0=00000010 CR2=00000000 CR3=00000000 CR4=00000000
 DR0=0000000000000000 DR1=0000000000000000 DR2=0000000000000000 DR3=0000000000000000
 DR6=00000000ffff0ff0 DR7=0000000000000400
 CCS=00000000 CCD=0000fea4 CCO=EFLAGS
-EFER=0000000000000000"
+EFER=0000000000000000",
     );
 
     let json_str = serde_json::to_string(&res.unwrap().0).unwrap();
     println!("{}", json_str);
 }
-
 
 #[cfg(test)]
 mod test {
@@ -248,21 +253,39 @@ mod test {
     fn general_regs() {
         let mut parser = general_regs_parser();
         let res = parser.parse("EAX=0000aa55 EBX=00000000 ECX=00000000 EDX=00000080\nESI=00000000 EDI=00000000 EBP=00000000 ESP=00006f2c\n").unwrap();
-        let expect = GeneralRegisters { EAX: 0xaa55, EBX: 0, ECX: 0, EDX: 0x80, ESI: 0, EDI: 0, EBP: 0, ESP: 0x6f2c };
+        let expect = GeneralRegisters {
+            EAX: 0xaa55,
+            EBX: 0,
+            ECX: 0,
+            EDX: 0x80,
+            ESI: 0,
+            EDI: 0,
+            EBP: 0,
+            ESP: 0x6f2c,
+        };
         assert_eq!(res, (expect, ""));
     }
 
     #[test]
     fn status_registers() {
         let mut parser = status_regs_parser();
-        let res = parser.parse("EIP=00007c00 EFL=00000202 [-------] CPL=0 II=0 A20=1 SMM=0 HLT=0\n");
-        assert_eq!(res.unwrap(), (StatusRegisters{
-            EIP: qword_parser().parse("EIP=00007c00").unwrap().0,
-            EFLAGS_RAW: qword_parser().parse("EFL=00000202").unwrap().0,
-            EFLAGS: eflags_parser().parse("[-------]").unwrap().0,
-            HFLAGS: count::<Vec<HFlag>, _>(5, hflag_parser()).parse("CPL=0 II=0 A20=1 SMM=0 HLT=0").unwrap().0,
-            },
-            ""));
+        let res =
+            parser.parse("EIP=00007c00 EFL=00000202 [-------] CPL=0 II=0 A20=1 SMM=0 HLT=0\n");
+        assert_eq!(
+            res.unwrap(),
+            (
+                StatusRegisters {
+                    EIP: qword_parser().parse("EIP=00007c00").unwrap().0,
+                    EFLAGS_RAW: qword_parser().parse("EFL=00000202").unwrap().0,
+                    EFLAGS: eflags_parser().parse("[-------]").unwrap().0,
+                    HFLAGS: count::<Vec<HFlag>, _>(5, hflag_parser())
+                        .parse("CPL=0 II=0 A20=1 SMM=0 HLT=0")
+                        .unwrap()
+                        .0,
+                },
+                ""
+            )
+        );
     }
 
     #[test]
@@ -276,16 +299,28 @@ mod test {
     #[test]
     fn descriptor_tables() {
         let mut parser = descriptor_tables_parser();
-        let res = parser.parse("GDT=     000f6c00 00000037\nIDT=     00000000 000003ff\n").unwrap();
-        let expect = DescriptorTable { GDT: (0xf6c00, 0x37), IDT: (0x0, 0x3ff) };
+        let res = parser
+            .parse("GDT=     000f6c00 00000037\nIDT=     00000000 000003ff\n")
+            .unwrap();
+        let expect = DescriptorTable {
+            GDT: (0xf6c00, 0x37),
+            IDT: (0x0, 0x3ff),
+        };
         assert_eq!(res, (expect, ""));
     }
 
     #[test]
     fn control_regs() {
         let mut parser = control_regs_parser();
-        let res = parser.parse("CR0=00000010 CR2=00000000 CR3=00000000 CR4=00000000\n").unwrap();
-        let expect = ControlRegs { CR0: 0x10, CR1: 0, CR2: 0, CR3: 0, };
+        let res = parser
+            .parse("CR0=00000010 CR2=00000000 CR3=00000000 CR4=00000000\n")
+            .unwrap();
+        let expect = ControlRegs {
+            CR0: 0x10,
+            CR1: 0,
+            CR2: 0,
+            CR3: 0,
+        };
         assert_eq!(res, (expect, ""));
     }
 
@@ -293,14 +328,23 @@ mod test {
     fn debug_regs() {
         let mut parser = debug_regs_parser();
         let res = parser.parse("DR0=0000000000000000 DR1=0000000000000000 DR2=0000000000000000 DR3=0000000000000000\nDR6=00000000ffff0ff0 DR7=0000000000000400\n").unwrap();
-        let expect = DebugRegs { DR0: 0, DR1: 0, DR2: 0, DR3: 0, DR6: 0xffff0ff0, DR7: 0x400 };
+        let expect = DebugRegs {
+            DR0: 0,
+            DR1: 0,
+            DR2: 0,
+            DR3: 0,
+            DR6: 0xffff0ff0,
+            DR7: 0x400,
+        };
         assert_eq!(res, (expect, ""));
     }
 
     #[test]
     fn qemu_internal() {
         let mut parser = skip_until(newline()).skip(newline());
-        let res = parser.parse("CCS=00000000 CCD=0000fea4 CCO=EFLAGS\n").unwrap();
+        let res = parser
+            .parse("CCS=00000000 CCD=0000fea4 CCO=EFLAGS\n")
+            .unwrap();
 
         assert_eq!(res, ((), ""));
     }
--- qemu-from/src/register_parser.rs
+++ qemu-from/src/register_parser.rs
@@ -1,56 +1,60 @@
-use ::{HFlag};
-use combine::{Parser, Stream, many1, token, one_of, between, count};
-use combine::parser::repeat::skip_until;
-use combine::char::{hex_digit, letter, spaces, alpha_num, string};
+use combine::char::{alpha_num, hex_digit, letter, spaces, string};
 use combine::error::ParseError;
+use combine::parser::repeat::skip_until;
+use combine::{between, count, many1, one_of, token, Parser, Stream};
+use HFlag;
 extern crate itertools;
 
 pub(crate) fn qword_parser<I>() -> impl Parser<Input = I, Output = u64>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let _id = spaces().with(skip_until(token('=')).skip(token('=')));
     let value = many1::<String, _>(hex_digit());
-    let parser = (_id, value)
-        .map(|(_id, value)| {
-            let value = u64::from_str_radix(&value, 16).expect("Fail to convert to u64");
-            value
-        });
+    let parser = (_id, value).map(|(_id, value)| {
+        let value = u64::from_str_radix(&value, 16).expect("Fail to convert to u64");
+        value
+    });
 
     parser
 }
 
 pub(crate) fn segment_parser<I>() -> impl Parser<Input = I, Output = (u64, u64, u64, u64)>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     use itertools::Itertools;
     let id = many1::<String, _>(letter()).skip(spaces()).skip(token('='));
-    let hex = spaces().with(many1::<String, _>(hex_digit()))
-        .map(|h| u64::from_str_radix(&h, 16).unwrap());
+    let hex = spaces()
+        .with(many1::<String, _>(hex_digit()))
+        .map(|h| u64::from_str_radix(&h, 16).expect("hexadevimal number."));
 
     let hex_list = count::<Vec<u64>, _>(4, hex)
-        .map(|hexes| hexes.into_iter().tuples::<(_,_,_,_)>().next().unwrap());
-    let parser = (id, hex_list)
-        .map(move |(_id, d)| d);
+        .map(|hexes| hexes.into_iter().tuples::<(_, _, _, _)>().next().unwrap());
+    let parser = (id, hex_list).map(move |(_id, d)| d);
 
     parser
 }
 
 pub(crate) fn hflag_parser<I>() -> impl Parser<Input = I, Output = HFlag>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let id = spaces().with(many1::<String, _>(alpha_num()).skip(token('=')));
-    let value = many1::<String, _>(hex_digit()).map(|h| u64::from_str_radix(&h, 16).unwrap());
-    let parser = (id, value).map(|(id, value)| { HFlag(id, value) });
+    let value = many1::<String, _>(
+        hex_digit()).map(|h| u64::from_str_radix(&h, 16).unwrap());
+    let parser = (id, value).map(|(id, value)| HFlag(id, value));
 
     parser
 }
 
 pub(crate) fn eflags_parser<I>() -> impl Parser<Input = I, Output = Vec<char>>
-    where I: Stream<Item = char>,
-          I::Error: ParseError<I::Item, I::Range, I::Position>,
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let eflag = many1::<Vec<char>, _>(one_of("DOSZAPC-".chars()));
     let parser = spaces().with(between(token('['), token(']'), eflag));
@@ -59,12 +63,17 @@ pub(crate) fn eflags_parser<I>() -> impl Parser<Input = I, Output = Vec<char>>
 }
 
 pub(crate) fn dt_parser<I>() -> impl Parser<Input = I, Output = (u64, u64)>
-    where I: Stream<Item = char>,
+where
+    I: Stream<Item = char>,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let id = string("GDT").or(string("IDT")).skip(token('='));
-    let value = || spaces().with(many1::<String, _>(hex_digit())
-        .map(|h| u64::from_str_radix(&h, 16).unwrap()));
+    let value = || {
+        spaces()
+        .with(many1::<String, _>(hex_digit())
+            .map(|h| u64::from_str_radix(&h, 16).unwrap())
+        )
+    };
     let value_pair = (value(), value());
 
     let parser = (id, value_pair).map(|(_, pair)| pair);
@@ -89,7 +98,10 @@ mod test {
 
     #[test]
     fn hflags() {
-        let res = count::<Vec<HFlag>, _>(5, hflag_parser()).parse("CPL=0 II=0 A20=1 SMM=0 HLT=0").unwrap().0;
+        let res = count::<Vec<HFlag>, _>(5, hflag_parser())
+            .parse("CPL=0 II=0 A20=1 SMM=0 HLT=0")
+            .unwrap()
+            .0;
         println!("{:?}", res);
     }
 }
