2e49e52ddbe94a97fb698bc0c3d48e5886f0c60e
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-16 21:38:02 +0900

    test: Success to read two cpu states.

--- qemu-from/tests/test_from_file.rs
+++ qemu-from/tests/test_from_file.rs
@@ -30,13 +30,34 @@ fn test_from_file_as_u8() {
     let mut reader = BufReader::new(file);
     let mut bytes = Vec::new();
     reader.read_to_end(&mut bytes).expect("fail to read from file.");
-    let buffer = String::from_utf8(bytes).unwrap();
+    let buffer = std::str::from_utf8(&bytes).unwrap();
 
     let mut parser = x86_cpu_state_parser();
-    let res = parser.parse(&buffer[..]);
+    let res = parser.parse(buffer);
 
     assert!(res.is_ok());
     let json_str = serde_json::to_string(&res.unwrap().0);
     assert!(json_str.is_ok());
     println!("{}", json_str.unwrap());
+}
+
+#[test]
+fn test_from_file_loop() {
+    let file = File::open("./tests/qemu_lines.log").expect("cannot open file");
+    let mut reader = BufReader::new(file);
+    let mut bytes = Vec::new();
+    reader.read_to_end(&mut bytes).expect("fail to read from file.");
+    let mut buffer = std::str::from_utf8(&bytes).unwrap();
+    let mut parser = x86_cpu_state_parser();
+
+    let mut count = 0;
+    while let Ok((result, remaining)) = parser.parse(buffer) {
+        let json_str = serde_json::to_string(&result);
+        assert!(json_str.is_ok());
+        println!("{}", json_str.unwrap());
+        buffer = remaining;
+        count += 1;
+    }
+
+    assert_eq!(count, 2);
 }
