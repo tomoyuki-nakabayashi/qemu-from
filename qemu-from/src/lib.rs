29eabce71f984dfd60accad388819d9ac35cb2ed
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-09-16 17:00:45 +0900

    test: Add integration test of test_from_string.

---
+++ qemu-from/src/lib.rs
@@ -0,0 +1,10 @@
+#[macro_use]
+extern crate combine;
+extern crate itertools;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+pub mod x86_cpu_state_parser;
+pub mod x86_element_parser;
--- qemu-from/tests/test_from_string.rs
+++ qemu-from/tests/test_from_string.rs
@@ -1,20 +1,15 @@
-#[macro_use]
+extern crate qemu_from;
 extern crate combine;
-extern crate itertools;
-extern crate serde;
 extern crate serde_json;
-#[macro_use]
-extern crate serde_derive;
 
-mod x86_element_parser;
-mod x86_cpu_state_parser;
+use qemu_from::x86_cpu_state_parser::x86_cpu_state_parser;
+use combine::Parser;
 
-use combine::{Parser};
-
-fn main() {
-    let mut parser = x86_cpu_state_parser::x86_cpu_state_parser();
+#[test]
+fn test_from_string() {
+    let mut parser = x86_cpu_state_parser();
     let res = parser.parse(
-        "EAX=0000aa55 EBX=00000000 ECX=00000000 EDX=00000080
+"EAX=0000aa55 EBX=00000000 ECX=00000000 EDX=00000080
 ESI=00000000 EDI=00000000 EBP=00000000 ESP=00006f2c
 EIP=00007c00 EFL=00000202 [-------] CPL=0 II=0 A20=1 SMM=0 HLT=0
 ES =0000 00000000 0000ffff 00009300
@@ -31,9 +26,12 @@ CR0=00000010 CR2=00000000 CR3=00000000 CR4=00000000
 DR0=0000000000000000 DR1=0000000000000000 DR2=0000000000000000 DR3=0000000000000000
 DR6=00000000ffff0ff0 DR7=0000000000000400
 CCS=00000000 CCD=0000fea4 CCO=EFLAGS
-EFER=0000000000000000",
+EFER=0000000000000000
+",
     );
 
-    let json_str = serde_json::to_string(&res.unwrap().0).unwrap();
-    println!("{}", json_str);
+    assert!(res.is_ok());
+    let json_str = serde_json::to_string(&res.unwrap().0);
+    assert!(json_str.is_ok());
+    println!("{}", json_str.unwrap());
 }
